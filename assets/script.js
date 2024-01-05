78a7a94cabe1649f83b50ff80bc1592a5800e09c
CestSamual <mvf4@hotmail.co.uk>
2024-01-05 19:30:23 +0000

    add pseudocode for draft of JS

--- assets/script.js
+++ assets/script.js
@@ -55,3 +55,26 @@ function retrieveCID() {
     });
 }
 retrieveCID();
+
+
+//ONS FETCH
+
+//Fetch function with WPC code from FTPC API.
+//uses pconValue in queryURL on ONS endpoint with area type as wpc.
+//produces json with the tenure data with dimention objects as the stat titles with each stat in an array called observations
+//target the observation array for the dimension of rent tenure
+//calculate as percentage by sum the array divided by the value * 100
+//this process will generate variables for the percentage, the total of rented and total of households
+//append these values to front end elements - will include a classList remove of d-none
+
+//eventListener for click of postcodeBtn to initiate ONS fetch function
+
+//MEMBERS CONTACT PARSE
+
+//target the email from members api fetch
+//parse and append into the mailto element to ensure the memebers email is located in the recipient address in users mail app 
+
+
+
+
+
