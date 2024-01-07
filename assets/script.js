aa5da71a060db040eed21a31986c82dc7d51ce83
CestSamual <mvf4@hotmail.co.uk>
2024-01-07 20:19:07 +0000

    add ons api JS

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,25 @@
-
+//Initialise Page
 document.addEventListener("DOMContentLoaded", function () {
   var postcodeInputEl = document.getElementById("postcodeInput");
   var postcodeBtnEl = document.getElementById("postcodeBtn");
   var mailToLinkEl = document.getElementById("mailToLink");
+  var areRentingEl = document.getElementById("areRenting");
+  var rentOutOfEl = document.getElementById("rentOutOf");
+  var peopleInAreaEl = document.getElementById("peopleInArea");
 
   
 
-
+  //Event Listener for postcode submission click
   postcodeBtnEl.addEventListener("click", function (event) {
     event.preventDefault(); 
 
     var postcode = postcodeInputEl.value;
     retrieveMemberID(postcode);
     retrieveCID(postcode);
+    retrieveONS();
+    
 })
-
+//Members API Member ID fetch
 function retrieveMemberID(postcode) {
   var queryURL =
     "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText=" +
@@ -34,7 +39,7 @@ function retrieveMemberID(postcode) {
       retrieveEmail(memberID);
     });
 }
-
+//Memebers API email fetch
 function retrieveEmail(memberID) {
   var queryURL =
     "https://members-api.parliament.uk/api/Members/" + memberID + "/Contact";
@@ -52,7 +57,7 @@ function retrieveEmail(memberID) {
 }
 
 
-
+//FTP API fetch
 function retrieveCID(postcode) {
   var queryURL = "https://findthatpostcode.uk//postcodes/" + postcode + ".json";
   // console.log(queryURL);
@@ -64,7 +69,34 @@ function retrieveCID(postcode) {
     .then(function (data) {
       var pconValue = data.data.attributes.pcon;
       console.log("pcon:", pconValue);
+      //Pass pconValue to retrieveONS function
+      retrieveONS(pconValue);
+      console.log(pconValue);
     });
+}
+
+//ONS API fetch
+function retrieveONS(pconValue){
+  var queryURL = "https://api.beta.ons.gov.uk/v1/datasets/TS054/editions/2021/versions/4/json?area-type=wpc," + pconValue;
+
+  fetch(queryURL)
+  .then(function (response){
+    return response.json();
+  })
+  .then(function (data) {
+    var statistics = data.observations;
+    //Calculation variables
+    var sumStat = 0;
+    for (var i = 0; i < statistics.length; i++){
+      sumStat += statistics[i];
+    };
+    var rentingStat = statistics[6] + statistics[7];
+    var rentingPercentStat = (rentingStat / sumStat) * 100;
+    //Parse to HTML elements
+    areRentingEl.innerHTML = rentingPercentStat.toFixed(0) + "%";
+    rentOutOfEl.innerHTML = rentingStat;
+    peopleInAreaEl.innerHTML = sumStat;
+  })
 }
 
 });
